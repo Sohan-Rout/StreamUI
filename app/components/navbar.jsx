2284e32977de71b87f04395f83bf54d354c5ba9b
Sohan Rout <sohanrout84@gmail.com>
2025-07-04 19:14:37 +0530

    added basic structure of navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,26 +1,43 @@
-import Link from "next/link";
-
 const navlinks = [
     { "title" : "Features", "links" : "#features" },
     { "title" : "Components" },
     { "title" : "Reviews" },
     { "title" : "FAQs" },
+];
+
+const social = [
+    { "title" : "Github", "links" : "/" },
+    { "title" : "LinkedIn", "links" : "/" },
+    { "title" : "ProductHunt", "links" : "/" },
 ]
 
 const Navbar = () => {
     return (
         <main>
-            <section>
+            <section className="flex flex-row justify-between">
             <div>
+                <h1 className="text-2xl">
+                    Stream<span className="bg-black text-white px-2 py-2 rounded-lg">Ui</span>
+                </h1>
             </div>
             <div>
+                <ul className="flex gap-4 text-lg">
                 {navlinks.map((item, index) => (
-                    <ul key={index}>
+                    <li key={index}>
                         <a href={item.links}>{item.title}</a>
-                    </ul>
+                    </li>
                 ))}
+                </ul>
             </div>
-            <div>
+            <div className="flex gap-2">
+                <p>Follow us on!</p>
+                <ul className="flex gap-4">
+                {social.map((item, index) => (
+                   <li key={index}>
+                    <a href={item.links}>{item.title}</a>
+                   </li> 
+                ))}
+                </ul>
             </div>
             </section>
         </main>
