093c83aec01e2a0475aaac6414a48c24c0552daa
Sohan Rout <sohanrout84@gmail.com>
2025-07-11 22:10:58 +0530

    chore : added og images

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,16 +5,16 @@ const domain = process.env.NEXT_PUBLIC_DOMAIN || "https://yourdomain.com";
 export const metadata = {
   title: "Stream UI – Beautiful, Copy-Paste UI Components for Your Projects",
   description:
-    "Stream UI offers a beautiful, accessible collection of reusable UI components ready to copy-paste into your projects, helping you build faster with consistent design.",
+    "Modern, developer-friendly React component library for your next project.",
   openGraph: {
     title: "Stream UI – Beautiful, Copy-Paste UI Components for Your Projects",
     description:
-      "Stream UI offers a beautiful, accessible collection of reusable UI components ready to copy-paste into your projects, helping you build faster with consistent design.",
+      "Modern, developer-friendly React component library for your next project.",
     url: `${domain}`,
     siteName: "Stream UI",
     images: [
       {
-        url: `${domain}/og-preview.png`,
+        url: `${domain}/ogImages/landing.png`,
         width: 1200,
         height: 630,
         alt: "Stream UI Preview",
