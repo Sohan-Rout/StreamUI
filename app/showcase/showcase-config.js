640aa066f83c5a479b4b1ee6a2a695715d6abab7
Sohan Rout <sohanrout84@gmail.com>
2025-07-12 09:38:56 +0530

    refactor : removed a function component

--- app/showcase/showcase-config.js
+++ app/showcase/showcase-config.js
@@ -6,7 +6,6 @@ import AnimatedDownloadButton from '@/app/showcase/components/elements/button3';
 import MinimalAvatar from '@/app/showcase/components/elements/avatar1';
 import RandomAvatar from '@/app/showcase/components/elements/avatar2';
 import BasicToggleSwitch from '@/app/showcase/components/elements/toggleSwitch1';
-import PushToggleButton from '@/app/showcase/components/elements/toggleSwitch2';
 import BasicGrid from '@/app/showcase/components/layout/grid1';
 import MinimalPricingLayout from '@/app/showcase/components/layout/pricing1';
 
@@ -613,55 +612,6 @@ export default function DownloadButton({ downloadLink, label = "Download" }) {
       { name: 'label', type: 'string', default: '"Download"', description: 'Text displayed on the button.' },
     ],
     category: 'Buttons',
-  },
-  {
-    slug: 'PushToggleButton',
-    title: '  Push Toggle Button',
-    description: 'A push-button to toggle dark and light modes globally for your app.',
-    preview: PushToggleButton,
-    code: `"use client";
-    import { useEffect, useState } from "react";
-    import { FaLightbulb } from "react-icons/fa";
-    
-    export default function DarkModeToggleButton() {
-      const [setIsDark] = useState(false);
-    
-      useEffect(() => {
-        // Initialize based on system or existing theme
-        if (
-          localStorage.theme === "dark" ||
-          (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
-        ) {
-          document.documentElement.classList.add("dark");
-          setIsDark(true);
-        }
-      }, []);
-    
-      const toggleTheme = () => {
-        if (document.documentElement.classList.contains("dark")) {
-          document.documentElement.classList.remove("dark");
-          localStorage.theme = "light";
-          setIsDark(false);
-        } else {
-          document.documentElement.classList.add("dark");
-          localStorage.theme = "dark";
-          setIsDark(true);
-        }
-      };
-    
-      return (
-        <button
-          onClick={toggleTheme}
-          className="p-2 rounded-full border border-neutral-300 dark:border-neutral-700 hover:bg-neutral-200 dark:hover:bg-neutral-700 transition shadow-lg shadow-black/25 dark:shadow-yellow-400/25 hover:scale-110 duration-300"
-          aria-label="Toggle Dark Mode"
-        >
-            <FaLightbulb className="dark:text-yellow-400 w-5 h-5 text-black" />
-        </button>
-      );
-    }`,
-    implementation: `<DarkModeToggleButton />`,
-    props: [],
-    category: 'Utilities',
   },
 {
     slug: 'BasicGrid',
